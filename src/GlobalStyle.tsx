c3009aaeb5e05a4a1cb031924034eb8aac33f1b7
onezeun <onezeun@gmail.com>
2024-07-04 01:13:42 +0900

    GlobalStyle

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+// import reset from 'styled-reset';
+// ${reset}
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  };
+
+  body {
+    font-family: "Pretendard-Regular", "Arial", sans-serif;
+    line-height: 1.5;
+  };
+
+  /* 스크롤바 */
+  ::-webkit-scrollbar {
+    width: 0.4rem;
+  };
+
+  ::-webkit-scrollbar-track {
+    background: transparent;
+  };
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #d1d5db;
+    border-radius: 0.25rem;
+  };
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #6b7280;
+  };
+`;
+
+export default GlobalStyle;
